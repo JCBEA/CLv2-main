2a548cc292f6ee21a2687f651ce355cf90601fc1
Alro John <alromercado08@gmail.com>
2024-10-08 10:57:05 +0800

    added creative services components

---
+++ src/app/(user)/creative-services/page.tsx
@@ -0,0 +1,9 @@
+import { ServiceHeroPage } from "./service-components/ServiceHeroPage";
+
+export default function CreativeServices() {
+    return (
+        <main className="w-full h-fit text-primary-2">
+            <ServiceHeroPage />
+        </main>
+    )
+}
--- src/app/(user)/directory/creative-services/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function CreativeServicesPage() {
-    return (
-        <main className="w-full h-fit text-primary-2">
-            asd
-        </main>
-    );
-}
